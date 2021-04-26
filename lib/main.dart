f765304a906142549863e876343e126c11e40a23
LouisAtVonage <louis.runcieman@vonage.com>
2021-04-26 19:13:05 +0100

    Homepage buttons into column. Centered vertically. Added font family from googlefonts. Defined default colours.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:workout_buddy/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,6 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: HomePageWidget(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Color.fromRGBO(220, 171, 18, 1.0),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.black,
+              minimumSize: Size(100.0, 50.0),
+            )
+        ),
+        fontFamily: GoogleFonts.robotoSlab().fontFamily,
+        // fontFamily: 'Raleway',
+
+      ),
     );
   }
 }
