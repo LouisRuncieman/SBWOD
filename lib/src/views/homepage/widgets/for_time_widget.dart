4cc3d42cc128122d7b113d9108d6fc3693074f8e
LouisAtVonage <louis.runcieman@vonage.com>
2021-04-28 22:49:25 +0100

    Setup routing

--- lib/src/views/homepage/widgets/for_time_widget.dart
+++ lib/src/views/homepage/widgets/for_time_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:workout_buddy/src/views/stopwatch.dart';
-import 'package:workout_buddy/src/theme/colors.dart';
-import 'package:workout_buddy/src/theme/themes.dart';
+import 'package:workout_buddy/src/app/colors.dart';
+import 'package:workout_buddy/src/app/themes.dart';
 
 class ForTimeWidget extends StatelessWidget {
   const ForTimeWidget({
@@ -22,10 +21,7 @@ class ForTimeWidget extends StatelessWidget {
                 borderRadius: BorderRadius.circular(circularBorderRadius),
               ),
               onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Stopwatch())
-                );
+                Navigator.pushNamed(context, '/stopwatch');
               },
               child: Padding(
                   padding: EdgeInsets.all(20),
